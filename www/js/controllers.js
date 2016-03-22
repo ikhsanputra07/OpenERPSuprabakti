7c56e7b985cc080474f8415d78a44c391eea522a
Mohammadrezafah <mrfahlepi@gmail.com>
2016-03-22 10:35:23 +0700

    login di form login baru

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,46 +14,47 @@ angular.module('app.controllers', [])
     // jsonp('http://10.36.15.51:8000/custom/get/?format=json');
 
 
-	// $scope.data = {};
- //    var name =(window.localStorage.getItem("dhaussjauhxdjuzlgzuglscfasshdausdjfkjzasd")) ;
- //    var pass =(window.localStorage.getItem("uhadlfdlfgghfrejajkfdfhzjudfakjhbfkjagfjufug")) ;
+	$scope.data = {};
+    var name =(window.localStorage.getItem("dhaussjauhxdjuzlgzuglscfasshdausdjfkjzasd")) ;
+    var pass =(window.localStorage.getItem("uhadlfdlfgghfrejajkfdfhzjudfakjhbfkjagfjufug")) ;
 
- //    if (name!=null && pass!=null){
+    if (name!=null && pass!=null){
 
        
- //         LoginService.loginUser(name, pass).success(function(data) {
- //            $state.go('activity');
- //        }).error(function(data) {
- //            var alertPopup = $ionicPopup.alert({
- //                title: 'Login failed!',
- //                template: 'Please check your credentials!1'
- //            });
- //        });
- //    }
- //    $scope.login = function() {
- //        console.log(window.btoa($scope.data.username))  
- //        LoginService.loginUser(window.btoa($scope.data.username),window.btoa($scope.data.pass)).success(function(data) {
- //            $state.go('activity');
+         LoginService.loginUser(name, pass).success(function(data) {
+            $state.go('sALESACTIVITY');
+        
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!1'
+            });
+        });
+    }
+    $scope.login = function() {
+        console.log(window.btoa($scope.data.username))  
+        LoginService.loginUser(window.btoa($scope.data.username),window.btoa($scope.data.pass)).success(function(data) {
+            $state.go('sALESACTIVITY');
 
          
            
- //            window.localStorage.setItem("dhaussjauhxdjuzlgzuglscfasshdausdjfkjzasd",window.btoa($scope.data.username))
- //            window.localStorage.setItem("uhadlfdlfgghfrejajkfdfhzjudfakjhbfkjagfjufug",window.btoa($scope.data.pass))
+            window.localStorage.setItem("dhaussjauhxdjuzlgzuglscfasshdausdjfkjzasd",window.btoa($scope.data.username))
+            window.localStorage.setItem("uhadlfdlfgghfrejajkfdfhzjudfakjhbfkjagfjufug",window.btoa($scope.data.pass))
           
- //        }).error(function(data) {
- //            var alertPopup = $ionicPopup.alert({
- //                title: 'Login failed!',
- //                template: 'Please check your credentials!2'
- //            });
- //        });
- //    }
-    // $scope.test_service = function() {
-    //     $ionicPopup.alert({
-    //         title:"Errorrr",
-    //         template:"testttt dulu bos"
-    //     });
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!2'
+            });
+        });
+    }
+    $scope.test_service = function() {
+        $ionicPopup.alert({
+            title:"Errorrr",
+            template:"testttt dulu bos"
+        });
 
-    // }
+    }
 })
 
    
